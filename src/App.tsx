d961a68b57e309c086c5e26e70b5a1bb9a94aff0
Sahil Gupta <guptas3067@gmail.com>
2025-08-14 20:53:03 +0530

    w

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './AppComponents.css'
 
-function Apps() {
+function Appps() {
   const [count, setCount] = useState(0)
 
   return (
